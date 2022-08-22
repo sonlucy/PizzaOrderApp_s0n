e6bdd0515dd8926956db4fcdd4f528e57d1aa407
s0n <shson971029@naver.com>
2022-08-22 16:51:37 +0900

    탭레이아웃 + 뷰페이저 배치

--- app/src/main/java/com/example/pizzaorderapp_s0n/MainActivity.kt
+++ app/src/main/java/com/example/pizzaorderapp_s0n/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.pizzaorderapp_s0n
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,6 +21,11 @@ class MainActivity : BaseActivity() {
     override fun setValues() {
         // 화면에 데이터를 표시하기 위한 코드 모음.
 
+        //뷰페이저 작업
+
+
+        //탭레이아웃과 연결
+        mainTabLayout.setupWithViewPager()
     }
 
 
