4d774f5f2621e2a67ed7edcf640c9d8807dce744
s0n <shson971029@naver.com>
2022-08-22 17:28:34 +0900

    뷰페이저 + 탭레이아웃 연결까지

--- app/src/main/java/com/example/pizzaorderapp_s0n/MainActivity.kt
+++ app/src/main/java/com/example/pizzaorderapp_s0n/MainActivity.kt
@@ -2,9 +2,13 @@ package com.example.pizzaorderapp_s0n
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pizzaorderapp_s0n.adapters.MainViewPagerAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
+
+    lateinit var mvpa : MainViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,10 +26,11 @@ class MainActivity : BaseActivity() {
         // 화면에 데이터를 표시하기 위한 코드 모음.
 
         //뷰페이저 작업
+        mvpa = MainViewPagerAdapter(supportFragmentManager)  //이렇게 만든 어댑터를
+        mainViewPager.adapter = mvpa  //mainViewPager의 어댑터로 mvpa가 동작할거야
 
-
-        //탭레이아웃과 연결
-        mainTabLayout.setupWithViewPager()
+        //탭레이아웃과 뷰페이저를 연결
+        mainTabLayout.setupWithViewPager(mainViewPager)
     }
 
 
