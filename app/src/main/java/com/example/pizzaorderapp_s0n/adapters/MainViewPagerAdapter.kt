a1d00e18ab3e06aceb110dd95287765e09c45f0f
s0n <shson971029@naver.com>
2022-08-22 17:17:55 +0900

    뷰 페이저용 어댑터 클래스 작성

---
+++ app/src/main/java/com/example/pizzaorderapp_s0n/adapters/MainViewPagerAdapter.kt
@@ -0,0 +1,24 @@
+package com.example.pizzaorderapp_s0n.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.example.pizzaorderapp_s0n.fragments.ChickenStoreListFragment
+import com.example.pizzaorderapp_s0n.fragments.PizzaStoreListFragment
+
+class MainViewPagerAdapter(fm: FragmentManager) :FragmentPagerAdapter(fm) {
+
+    override fun getCount(): Int {
+        //총 몇장짜리인가요
+        return 2
+    }
+
+    override fun getItem(position: Int): Fragment {
+        //포지션마다 어떤 프래그먼트가 나가야하나요
+        return when(position){
+            0 -> PizzaStoreListFragment()
+            else -> ChickenStoreListFragment()
+
+        }
+    }
+}
