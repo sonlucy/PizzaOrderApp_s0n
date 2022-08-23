5ef7fb837dd2fcdb21fc54d747dac15886fef301
s0n <shson971029@naver.com>
2022-08-23 23:03:07 +0900

    상세 화면의 실제 데이터 반영

--- app/src/main/java/com/example/pizzaorderapp_s0n/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/example/pizzaorderapp_s0n/ViewStoreDetailActivity.kt
@@ -2,8 +2,13 @@ package com.example.pizzaorderapp_s0n
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.example.pizzaorderapp_s0n.datas.Store
+import kotlinx.android.synthetic.main.activity_view_store_detail.*
 
 class ViewStoreDetailActivity : BaseActivity() {
+
+    lateinit var mStoreData : Store
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +24,14 @@ class ViewStoreDetailActivity : BaseActivity() {
 
     override fun setValues() {
 
+        //여기서 집어넣기
+        mStoreData = intent.getSerializableExtra("storeData") as Store
+
+        Glide.with(mContext).load(mStoreData.logoURL).into(storeLogoImg)  //베이스액티비티 mContext. / logo주소를 storeLogoImg에 뿌릴거임
+
+        //가게이름, 전화번호
+        storePhoneNumTxt.text = mStoreData.phoneNum
+        storeNameTxt.text=mStoreData.name
     }
 
 
